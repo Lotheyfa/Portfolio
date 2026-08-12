50d8c5f9c0c499c8be68ea9de24549e801e762d8
Lotheyfa <shatsi99@gmail.com>
2026-08-12 12:44:26 +0300

    added darkmode button

---
+++ script.js
@@ -0,0 +1,15 @@
+const darkModeButton = document.getElementById("darkModeButton");
+
+darkModeButton.addEventListener("click", function() {
+    document.body.classList.toggle("dark-mode");
+
+    if (document.body.classList.contains("dark-mode")) {
+        localStorage.setItem("darkMode", "enabled");
+    } else {
+        localStorage.setItem("darkMode", "disabled");
+    }
+});
+
+if (localStorage.getItem("darkMode") === "enabled") {
+    document.body.classList.add("dark-mode");
+}
